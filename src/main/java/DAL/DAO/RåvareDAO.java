8fd3e70f2253f3ac11f0f0c0b3d81238b4840c5b
Joe-Briefcase <s185143@student.dtu.dk>
2019-04-30 12:46:31 +0200

    Oprettet DAO'er.

--- src/main/java/DAL/DAO/RåvareDAO.java
+++ src/main/java/DAL/DAO/RåvareDAO.java
@@ -1,15 +1,25 @@
 package DAL.DAO;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class RåvareDAO implements IRåvareDAO {
 
     public void createRåvare(Connection connection, IRåvareDTO råvareDTO) {
-
+        try {
+            PreparedStatement statement = connection.prepareStatement("INSERT into Råvare values(?,?,?,?,?);");
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public void getRåvare(Connection connection, int ID) {
-
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM Råvare WHERE ID = ?;")
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public void updateRåvare(Connection connection, IRåvareDTO råvareDTO) {
